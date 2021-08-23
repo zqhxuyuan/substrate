aeb6e67fd53163c75f69d12c67df2a96b07f9df0
zqh <zqhxuyuan@gmail.com>
2021-08-23 17:46:20 +0800

    rm election-multi-phase

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -307,7 +307,7 @@ impl pallet_authorship::Config for Runtime {
 	type FindAuthor = pallet_session::FindAccountFromAuthorIndex<Self, Babe>;
 	type UncleGenerations = UncleGenerations;
 	type FilterUncle = ();
-	type EventHandler = (Staking);
+	type EventHandler = (Staking, ImOnline);
 }
 
 impl_opaque_keys! {
