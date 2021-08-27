32f64702c4497b38ebb537a965fb46e5e653f96c
zqh <zqhxuyuan@gmail.com>
2021-08-27 14:39:15 +0800

    first accounts works

--- frame/accounts/src/lib.rs
+++ frame/accounts/src/lib.rs
@@ -16,6 +16,7 @@ use sp_std::{
 
 pub use pallet::*;
 use frame_support::traits::Get;
+use frame_support::sp_runtime::app_crypto::sp_core::sp_std::cmp::Ordering;
 
 #[frame_support::pallet]
 pub mod pallet {
@@ -80,6 +81,15 @@ pub mod pallet {
 				// todo: only one owner and one active most, use another storage item for check
 				let mut permissions_old =
 					PermissionMap::<T>::get(&sender).ok_or(DispatchError::CannotLookup)?;
+				let perm_types: Vec<PermType> = permissions_old.iter()
+					.map(|(x,_)| *x).collect::<Vec<PermType>>();
+				log::info!("exist perm types:{:?}", perm_types);
+				let perm_exist = perm_types.contains(&perm_name);
+				if perm_exist {
+					// todo: error
+					log::warn!("perm type exist");
+					return Ok(())
+				}
 				permissions_old.try_push((perm_name, permission_auth_data));
 				PermissionMap::<T>::try_mutate(sender, |permissions|->DispatchResult {
 					match permissions {
@@ -102,41 +112,177 @@ pub mod pallet {
 		}
 
 		#[pallet::weight(0)]
-		pub fn update_permission(origin: OriginFor<T>,
+		pub fn add_or_update_permission(origin: OriginFor<T>,
+										perm_name: PermType,
+										perm_module: PermModule,
+										perm_effect: Effect,
+										perm_method: Option<[u8; 5]>
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			let mut permissions_old =
+				PermissionMap::<T>::get(&sender).ok_or(DispatchError::CannotLookup)?;
+
+			let mut permission_new = BoundedVec::<OnePermissionData, T::MaxPermission>::default();
+			let current_permission_data = OnePermissionData {
+				effect: perm_effect,
+				module: perm_module,
+				method: perm_method
+			};
+			permission_new.try_push(current_permission_data);
+			for (perm_type, perm_data) in permissions_old {
+				if perm_type.eq(&perm_name) {
+					for perm in perm_data.permissions {
+						if !perm.module.eq(&perm_module) {
+							permission_new.try_push(perm);
+						}
+					}
+					let current_permission_auth_data = PermissionAndOwnerData {
+						perm_type: perm_name,
+						parent_perm_type: perm_data.parent_perm_type,
+						permissions: permission_new,
+						threshold: perm_data.threshold,
+						auths: perm_data.auths
+					};
+					// todo: use try_mutate?
+					PermissionMap::<T>::remove(&sender);
+					let permission_vec = vec![(perm_name, current_permission_auth_data)];
+					let permission_vec =
+						BoundedVec::<_, T::MaxOthers>::try_from(permission_vec).map_err(|_| DispatchError::CannotLookup)?;
+					PermissionMap::<T>::insert(&sender, permission_vec);
+					return Ok(())
+				}
+			}
+			Ok(())
+		}
+
+		#[pallet::weight(0)]
+		pub fn add_or_update_auth(origin: OriginFor<T>,
+								  perm_name: PermType,
+								  account_key: AccountOrKey,
+								  weight: u32,
+								  threshold: u32,
+		) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			let mut permissions_old =
+				PermissionMap::<T>::get(&sender).ok_or(DispatchError::CannotLookup)?;
+
+			let mut latest_data = BoundedVec::<OneAuthData, T::MaxAuth>::default();
+			let current_data = OneAuthData {
+				account_key,
+				weight
+			};
+			latest_data.try_push(current_data);
+			for (perm_type, perm_data) in permissions_old {
+				if perm_type.eq(&perm_name) {
+					for perm in perm_data.auths {
+						if !perm.account_key.eq(&account_key) {
+							latest_data.try_push(perm);
+						}
+					}
+					let current_permission_auth_data = PermissionAndOwnerData {
+						perm_type: perm_name,
+						parent_perm_type: perm_data.parent_perm_type,
+						permissions: perm_data.permissions,
+						threshold,
+						auths: latest_data
+					};
+					// todo: use try_mutate?
+					PermissionMap::<T>::remove(&sender);
+					let permission_vec = vec![(perm_name, current_permission_auth_data)];
+					let permission_vec =
+						BoundedVec::<_, T::MaxOthers>::try_from(permission_vec).map_err(|_| DispatchError::CannotLookup)?;
+					PermissionMap::<T>::insert(&sender, permission_vec);
+					return Ok(())
+				}
+			}
+			Ok(())
+		}
+
+		#[pallet::weight(0)]
+		pub fn delete_permission(origin: OriginFor<T>,
 								 perm_name: PermType,
-			perm_model: PermModule,
-			perm_effect: Effect,
-			perm_method: Option<[u8; 10]>
+								 perm_modules: Vec<PermModule>,
 		) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
-			// let (perm_name, mut permissions_old)
-			// 	= PermissionMap::<T>::get(&sender).ok_or(DispatchError::CannotLookup)?;
+			let mut permissions_old =
+				PermissionMap::<T>::get(&sender).ok_or(DispatchError::CannotLookup)?;
+
+			let mut permission_new = BoundedVec::<OnePermissionData, T::MaxPermission>::default();
+			for (perm_type, perm_data) in permissions_old {
+				if perm_type.eq(&perm_name) {
+					let mut exist = false;
+					for perm in perm_data.permissions {
+						for perm_module in &perm_modules {
+							if !perm.module.eq(perm_module) {
+								permission_new.try_push(perm);
+							} else {
+								exist = true;
+							}
+						}
+					}
+					if !exist {
+						return Ok(())
+					}
+					let current_permission_auth_data = PermissionAndOwnerData {
+						perm_type: perm_name,
+						parent_perm_type: perm_data.parent_perm_type,
+						permissions: permission_new,
+						threshold: perm_data.threshold,
+						auths: perm_data.auths
+					};
+					// todo: use try_mutate?
+					PermissionMap::<T>::remove(&sender);
+					let permission_vec = vec![(perm_name, current_permission_auth_data)];
+					let permission_vec =
+						BoundedVec::<_, T::MaxOthers>::try_from(permission_vec).map_err(|_| DispatchError::CannotLookup)?;
+					PermissionMap::<T>::insert(&sender, permission_vec);
+					return Ok(())
+				}
+			}
 			Ok(())
 		}
 
 		#[pallet::weight(0)]
-		pub fn add_permission(origin: OriginFor<T>,
-							  perm_name: PermType,
-							  perm_data: Vec<OnePermissionData>
-		) -> DispatchResult {
-			let sender = ensure_signed(origin)?;
-
-			Ok(())
-		}
-
-		#[pallet::weight(0)]
-		pub fn update_auth(origin: OriginFor<T>,
+		pub fn delete_auth(origin: OriginFor<T>,
 						   perm_name: PermType,
+						   account_keys: Vec<AccountOrKey>,
 		) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
-			Ok(())
-		}
+			let mut permissions_old =
+				PermissionMap::<T>::get(&sender).ok_or(DispatchError::CannotLookup)?;
 
-		#[pallet::weight(0)]
-		pub fn add_auth(origin: OriginFor<T>,
-						perm_name: PermType,
-		) -> DispatchResult {
-			let sender = ensure_signed(origin)?;
+			let mut data_new = BoundedVec::<OneAuthData, T::MaxAuth>::default();
+			for (perm_type, perm_data) in permissions_old {
+				if perm_type.eq(&perm_name) {
+					let mut exist = false;
+					for data in perm_data.auths {
+						for account_key in &account_keys {
+							if !data.account_key.eq(account_key) {
+								data_new.try_push(data);
+							} else {
+								exist = true;
+							}
+						}
+					}
+					if !exist {
+						return Ok(())
+					}
+					let current_permission_auth_data = PermissionAndOwnerData {
+						perm_type: perm_name,
+						parent_perm_type: perm_data.parent_perm_type,
+						permissions: perm_data.permissions,
+						threshold: perm_data.threshold,
+						auths: data_new
+					};
+					// todo: use try_mutate?
+					PermissionMap::<T>::remove(&sender);
+					let permission_vec = vec![(perm_name, current_permission_auth_data)];
+					let permission_vec =
+						BoundedVec::<_, T::MaxOthers>::try_from(permission_vec).map_err(|_| DispatchError::CannotLookup)?;
+					PermissionMap::<T>::insert(&sender, permission_vec);
+					return Ok(())
+				}
+			}
 			Ok(())
 		}
 	}
@@ -187,31 +333,31 @@ pub mod pallet {
 	}
 }
 
-#[derive(Encode, Decode, Clone, Copy, PartialEq, Eq, RuntimeDebug, MaxEncodedLen)]
+#[derive(Encode, Decode, Clone, Copy, PartialEq, Eq, RuntimeDebug, MaxEncodedLen, Ord, PartialOrd)]
 pub enum PermType {
 	Owner,
 	Active,
-	Others([u8; 10]),
+	Others([u8; 5]),
 }
 #[derive(Encode, Decode, Clone, Copy, PartialEq, Eq, RuntimeDebug, MaxEncodedLen)]
 pub enum Effect {
 	Positive,
 	Negative,
 }
-#[derive(Encode, Decode, Clone, Copy, PartialEq, Eq, RuntimeDebug, MaxEncodedLen)]
+#[derive(Encode, Decode, Clone, Copy, PartialEq, Eq, RuntimeDebug, MaxEncodedLen, Ord, PartialOrd)]
 pub enum PermModule {
 	ALL,
 	SELF,
-	Module([u8; 10])
+	Module([u8; 5])
 }
 #[derive(Encode, Decode, Clone, Copy, PartialEq, Eq, RuntimeDebug, MaxEncodedLen)]
 pub enum AccountOrKey {
-	Account([u8; 10]),
-	Key([u8; 10])
+	Account([u8; 5]),
+	Key([u8; 5])
 }
 
 #[derive(
-	CloneNoBound, Encode, Decode, Eq, MaxEncodedLen, PartialEqNoBound, RuntimeDebugNoBound,
+	CloneNoBound, Encode, Decode, Eq, MaxEncodedLen, PartialEqNoBound, RuntimeDebugNoBound
 )]
 #[codec(mel_bound(MaxPerm: Get<u32>, MaxAuth: Get<u32>))]
 #[cfg_attr(test, derive(frame_support::DefaultNoBound))]
@@ -228,16 +374,16 @@ pub struct PermissionAndOwnerData<MaxPerm: Get<u32>, MaxAuth: Get<u32>> {
 	pub auths: BoundedVec<OneAuthData, MaxAuth>,
 }
 
-#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, MaxEncodedLen)]
+#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, MaxEncodedLen, Copy)]
 pub struct OnePermissionData {
 	pub effect: Effect,
 	// module name: self or other module/contract
 	pub module: PermModule,
 	// method name: if module is All, the method is None
 	// if one module has many method, this will related to multi record
-	pub method: Option<[u8; 10]>,
+	pub method: Option<[u8; 5]>,
 }
-#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, MaxEncodedLen)]
+#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, MaxEncodedLen, Copy)]
 pub struct OneAuthData {
 	pub account_key: AccountOrKey,
 	pub weight: u32
